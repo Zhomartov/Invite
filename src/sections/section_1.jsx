3034ff567162c8b0eb2c1df1dff62d7cd76e0232
Olzhas <zhomartov_olzhas@mail.ru>
2025-10-16 17:24:09 +0500

    Build-5

--- src/sections/section_1.jsx
+++ src/sections/section_1.jsx
@@ -7,7 +7,7 @@ const Section_1 = () => {
 	return(
 		<div className='lg:max-h-[1000px] h-full p-2 flex flex-col items-center justify-start relative overflow-hidden w-full'>
 		<Logo />
-		<motion.span initial={{opacity: 0}} animate={{opacity:1}} transition={{delay:4 , type:'spring'}} className='text-[15em] uppercase text-[#8B0000] font-main'>{t('logo_text')}</motion.span>
+		<motion.span initial={{opacity: 0}} animate={{opacity:1}} transition={{delay:4 , type:'spring'}} className='text-[10em] lg:text-[15em] uppercase text-[#8B0000] font-main'>{t('logo_text')}</motion.span>
 		</div>
 	);
 }
